a94e294f56b0a7a8468c626335a14f04f85bb18f
BaizaOP <zayn.baig@gmail.com>
2021-06-10 19:35:23 -0400

    Started Codeforces entry 272A on 6/10/2021 in SUMMER CHALLENGE 2021!

---
+++ summer2021/0272/a.cpp
@@ -0,0 +1,36 @@
+// Author: BaizaOP
+// Date: 6/10/2021
+// Problem Name: 272A Dima and Friends
+// Problem Difficulty: 1000
+// Summer 2021 Challenge Number: 1
+// Number Today: 1
+// Number of attempts: 
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n; cin >> n;
+    int sum = 0;
+    for(int i = 0; i < n; i++)
+    {
+        int p; cin >> p;
+        sum += p;
+    }
+
+    int retnum = 0;
+    n++; //include dima
+    for(int i = 1; i <=5; i++) 
+    {
+        int x = sum;
+        x+=i;
+        if(x % n != 1)
+        {
+            retnum++;
+        }
+    }
+    cout << retnum << endl;
+    return 0;
+}
---
+++ summer2021/newtemplate.cpp
@@ -0,0 +1,15 @@
+// Author: BaizaOP
+// Date:
+// Problem Name:
+// Problem Difficulty:
+// Summer 2021 Challenge Number: 
+// Number Today:
+// Number of attempts: 
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+}
