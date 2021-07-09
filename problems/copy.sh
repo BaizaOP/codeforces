13a86b241b750ca7f9c046893219c84722f25744
BaizaOP <zayn.baig@gmail.com>
2021-07-09 01:41:18 -0400

    I changed copy.sh so it won't RESET my home directory

--- problems/copy.sh
+++ problems/copy.sh
@@ -2,5 +2,5 @@
 
 # A simple copy script of a file to a shared directory for my host os to recognize a file I am working on in WSL Ubuntu
 
-rm -rf $2/*
-cp $1 $2
+rm -rf $jointoses/*
+cp $1 $jointoses
